963829e34a83067877dedadb0d53d1532cdb02f0
Olga <olgasambulova21@gmail.com>
2026-04-27 19:31:06 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
   return arr;
+}
+  if (d === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+    return arr;
+  }
+  if (d > 0) {
+    let root1 = (-b + Math.sqrt(d)) / (2 * a);
+    let root2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(root1, root2);
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
