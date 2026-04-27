3a2094c6b92feee4da58d2291ec996bec23ef6c4
Olga <olgasambulova21@gmail.com>
2026-04-27 20:12:19 +0300

    Реализовано второе задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,5 +19,15 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = percent / 100 / 12;
+  let S = amount - contribution;
+
+  if (S <= 0) {
+    return 0;
+  }
+  let n = countMonths;
+  let monthlyPayment = S * (P + (P / (Math.pow(1 + P, n) - 1)));
+  let totalAmount = monthlyPayment * n;
+  totalAmount = Math.round(totalAmount * 100) / 100;
+  return totalAmount;
 }
