8938a0d1c4a4061727dc7b4e2964a2078b7d59d4
Olga <olgasambulova21@gmail.com>
2026-05-11 19:37:36 +0300

    код преобразован в соотвествии с заданием по теме "Классы"

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,30 +1,47 @@
-function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-  this.marks = [];
-}
-
-Student.prototype.setSubject = function (subjectName) {
-   this.subject = subjectName;
-}
-
-Student.prototype.addMarks = function (...marks) {
-   if (this.marks && !this.excluded) {
-    this.marks.push(...marks);
+class Student {
+  constructor(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = {};
   }
-}
 
-Student.prototype.getAverage = function () {
-  if (!this.marks || this.marks.length === 0) {
-    return 0;
+  setSubject(subjectName) {
+    this.subject = subjectName;
   }
-  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
-  return sum / this.marks.length;
-}
 
-Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
+  addMark(mark, subject) {
+    if (mark < 2 || mark > 5) {
+      return;
+    }
+    if (!this.marks[subject]) {
+      this.marks[subject] = [];
+    }
+    this.marks[subject].push(mark);
+  }
+
+  getAverageBySubject(subject) {
+    if (!this.marks[subject] || this.marks[subject].length === 0) {
+      return 0;
+    }
+    const sum = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks[subject].length;
+  }
+
+  getAverage() {
+    const subjects = Object.keys(this.marks);
+    if (subjects.length === 0) {
+      return 0;
+    }
+    const sumOfAverages = subjects.reduce((acc, subject) => {
+      return acc + this.getAverageBySubject(subject);
+    }, 0);
+    return sumOfAverages / subjects.length;
+  }
+
+  exclude(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
 }
