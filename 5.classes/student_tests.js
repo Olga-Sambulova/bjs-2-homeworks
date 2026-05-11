799e7c4ae758c922c0dbe4fb299e62596e781f22
Olga <olgasambulova21@gmail.com>
2026-05-11 20:22:00 +0300

    Дополнительное задание готово

--- 5.classes/student_tests.js
+++ 5.classes/student_tests.js
@@ -1,59 +1,35 @@
-describe('Домашнее задание к лекции 5 «Классы». Дополнительное задание', () => {
-  describe('Задача №3', () => {
-    let student;
-  
-    beforeEach(function(){
-      student = new Student("Иван Петров");
-    });
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {};
+  }
 
-    it('создание объекта Student', () => {
-      expect(student).toBeDefined();
-      expect(student.name).toEqual("Иван Петров");
-      expect(student.marks).toEqual({});
-    });
+  addMark(mark, subject) {
+    if (mark < 2 || mark > 5) {
+      return;
+    }
+    if (!this.marks[subject]) {
+      this.marks[subject] = [];
+    }
+    this.marks[subject].push(mark);
+  }
 
-    it('добавление оценок по разным предметам', () => {
-      student.addMark(3, "математика");
-      expect(student.marks).toEqual({"математика": [3]});
-      student.addMark(5, "математика");
-      expect(student.marks).toEqual({"математика": [3, 5]});
-      student.addMark(5, "физика");
-      expect(student.marks).toEqual({"математика": [3, 5], "физика": [5]});
-    });
+  getAverageBySubject(subject) {
+    if (!this.marks[subject] || this.marks[subject].length === 0) {
+      return 0;
+    }
+    const sum = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks[subject].length;
+  }
 
-    it('невозможность добавления некорректных оценок', () => {
-      student.addMark(0, "математика");
-      expect(student.marks).toEqual({});
-      student.addMark(3, "математика");
-      expect(student.marks).toEqual({"математика": [3]});
-      student.addMark(10, "математика");
-      expect(student.marks).toEqual({"математика": [3]});
-      student.addMark(7, "физика");
-      expect(student.marks).toEqual({"математика": [3]});
-    });
-
-    it('подсчёт средней оценки по предмету', () => {
-      student.addMark(3, "математика");
-      student.addMark(5, "математика");
-      expect(student.getAverageBySubject("математика")).toEqual(4);
-    });
-
-    it('подсчёт средней оценки по несуществующему предмету', () => {
-      student.addMark(3, "математика");
-      student.addMark(5, "математика");
-      expect(student.getAverageBySubject("программирование")).toEqual(0);
-    });
-
-    it('подсчёт общей средней оценки пустого объекта оценок', () => {
-      expect(student.getAverage()).toEqual(0);
-    });
-
-    it('подсчёт общей средней оценки', () => {
-      student.addMark(3, "математика");
-      student.addMark(5, "математика");
-      student.addMark(5, "история");
-      student.addMark(5, "история");
-      expect(student.getAverage()).toEqual(4.5);
-    });
-  });
-});
+  getAverage() {
+    const subjects = Object.keys(this.marks);
+    if (subjects.length === 0) {
+      return 0;
+    }
+    const sumOfAverages = subjects.reduce((acc, subject) => {
+      return acc + this.getAverageBySubject(subject);
+    }, 0);
+    return sumOfAverages / subjects.length;
+  }
+}
